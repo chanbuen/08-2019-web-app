7e1e677e9e6e91557b964a87765e4235d4df6bc3
Buena Chan <buenachan@Buenas-MacBook-Pro-2.local>
2019-08-10 17:45:32 -0400

    updated nav bar

--- client/components/Nav/index.js
+++ client/components/Nav/index.js
@@ -3,11 +3,16 @@ import React, { Component } from 'react';
 export default class Nav extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li>Category 1</li>
-          <li>Category 2</li>
-          <li>Category 3</li>
+      <div className="navigation-container">
+        <ul className="navigation-container__ul">
+          <li className="navigation-item__li">Category 1</li>
+          <li className="navigation-item__li">Category 2</li>
+          <li className="navigation-item__li">Category 3</li>
+          <li className="navigation-item__li">Category 4</li>
+          <li className="navigation-item__li">Category 5</li>
+          <li className="navigation-item__li">Category 6</li>
+          <li className="navigation-item__li">Category 7</li>
+          <li className="navigation-item__li">Category 8</li>
         </ul>
       </div>
     )
