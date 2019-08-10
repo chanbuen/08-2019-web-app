97b8dae4fa26873dc19ace7fe67e0aa485507248
Buena Chan <buenachan@Buenas-MacBook-Pro-2.local>
2019-08-10 19:12:10 -0400

    added logo

--- client/components/Nav/index.js
+++ client/components/Nav/index.js
@@ -3,17 +3,24 @@ import React, { Component } from 'react';
 export default class Nav extends Component {
   render() {
     return (
-      <div className="navigation-container">
-        <ul className="navigation-container__ul">
-          <li className="navigation-item__li">Category 1</li>
-          <li className="navigation-item__li">Category 2</li>
-          <li className="navigation-item__li">Category 3</li>
-          <li className="navigation-item__li">Category 4</li>
-          <li className="navigation-item__li">Category 5</li>
-          <li className="navigation-item__li">Category 6</li>
-          <li className="navigation-item__li">Category 7</li>
-          <li className="navigation-item__li">Category 8</li>
-        </ul>
+      <div>
+        <div>
+          <div className="logo-container">
+          <img src={window.location.origin + '/images/logo.png'}></img>
+          </div>
+        </div>
+        <div className="navigation-container">
+          <ul className="navigation-container__ul">
+            <li className="navigation-item__li"><span>Category</span></li>
+            <li className="navigation-item__li"><span>Category</span></li>
+            <li className="navigation-item__li"><span>Category</span></li>
+            <li className="navigation-item__li"><span>Category</span></li>
+            <li className="navigation-item__li"><span>Category</span></li>
+            <li className="navigation-item__li"><span>Category</span></li>
+            <li className="navigation-item__li"><span>Category</span></li>
+            <li className="navigation-item__li"><span>Category</span></li>
+          </ul>
+        </div>
       </div>
     )
   }
